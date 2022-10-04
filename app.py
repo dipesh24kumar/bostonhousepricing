a99eb2fda22f571d208a3ceed2db2cc7f9d88e9d
Dipesh kumar <dipeshsingh2406@gmail.com>
2022-10-04 12:59:34 +0530

    This is the commit for web applications

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
+import json
 import pickle
-import os
+
 from flask import Flask,request,app,jsonify,url_for,render_template
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import pandas as pd
 
 app=Flask(__name__)
-## load the model
-linear_mod=pickle.load(open("Linear_model_boston.pkl",'rb'))
-scale_model=pickle.load(open("scaled_transfrom_model.pkl",'rb'))
-
+## Load the model
+regmodel=pickle.load(open('Linear_model_boston.pkl','rb'))
+scalar=pickle.load(open('scaling.pkl','rb'))
 @app.route('/')
 def home():
     return render_template('home.html')
+
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
     data=request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
-    ##new_data=np.array(list(data.values())).reshape(1,-1)
-    new_data=scale_model.transfrom(np.array(list(data.values())).reshape(1,-1)) ##new data first need to tranfrom
-    output=linear_mod.predict(new_data)
-    print(output)
+    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    output=regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
 if __name__=='__main__':
     app.run(debug=True)    
